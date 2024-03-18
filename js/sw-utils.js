8b3f49418ab3a3740cb0aefeda13550ac1068e2c
ASMED MEDICAL S.L.U <124255516+JAITsanchez@users.noreply.github.com>
2024-03-18 13:01:44 +0100

    first commit to github pages

---
+++ js/sw-utils.js
@@ -0,0 +1,11 @@
+const updateDynamicCache = (dynamicCache, req, res) => {
+
+  if (res.ok) {
+    caches.open(dynamicCache).then(cache => {
+      cache.put(req, res.clone());
+      return res.clone();
+    })
+  } else {
+    return res;
+  }
+}
